a6b16b4d74f6df13732e8833dc0bc6e565714874
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-07-04 16:47:18 -0300

    Create ex01.py

---
+++ ex01.py
@@ -0,0 +1,66 @@
+print('Hello World!') 
+print('-----------------------------------------\n') #\n pula linha
+
+# Somatório dos 5 primeiros números inteiros e positivos
+print(int(1 + 2 + 3+ 4 + 5))
+print()
+
+#A média entre 23, 19, 31
+print((23 + 19 + 31) / 3) # / com casas decimais
+print()
+
+#O número de vezes que 73 cabe em 403
+print(403 // 73) # // somente a parte inteira
+print()
+
+# A sobra quando 403 é dividido por 73
+print(403 % 73)
+print()
+
+# 2 elevado à 10° potência
+print(2 ** 10)
+print()
+
+# O valor absoluto da diferença entre 54 e 57
+print(abs(54 - 57))
+print()
+
+# O menor valor entre 34, 29 e 31
+print(min(34, 29, 31))
+print()
+
+# print-exercicio-aula2(img - 1)
+a = 3
+b = 4
+c = a * a + b * b
+print(c)
+print()
+
+# print-exercicio-aula2(img - 2)
+s1 = 'ant'
+s2 = 'bat'
+s3 = 'cod'
+
+# print-exercicio-aula2(img - 1)
+# a 
+print(s1, s2, s3) 
+print(s1 + ' ' + s2 + ' ' + ' ' + s3)
+print()
+
+#b
+print((s1 + ' ') * 10)
+print()
+
+#c
+res = (s1 + ' ' + ((s2 + ' ') * 2 ) + ' ' + ((s3 + ' ') * 3))
+print(res)
+print()
+
+#d 
+res = 7 * (s1 + ' ' + s2 + ' ')
+print(res)
+print()
+
+#e 
+res = 5 * (s2 * 2  + s3 + ' ')
+print(res)
