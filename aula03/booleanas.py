633bafde75d613f3bb12cdccad6b86b5a955244c
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-07-09 16:41:49 -0300

    Create booleanas.py

---
+++ aula03/booleanas.py
@@ -0,0 +1,24 @@
+print(2 + 2 < 4)
+print()
+
+if(7 // 3 == 1 +1):
+ print('Verdadeiro')
+ print()
+
+print(3 ** 2 + 4 **2 == 25)
+print()
+
+print(2 + 4  + 6 > 12)
+print()
+
+if(1387 % 19 == 0):
+ print('Verdadeiro')
+ print()
+
+print(31 % 2 == 0)
+print()
+
+if(min(34, 29, 31) < 30):
+ print('Verdadeiro')
+
+
