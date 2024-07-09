a638735b3e1a4c24f78f97791b1b0aaa27d930d6
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-07-09 16:40:11 -0300

    Create condicionais_simples.py

---
+++ aula03/condicionais_simples.py
@@ -0,0 +1,27 @@
+idade = 65
+
+if(idade > 60):
+    print('Você tem direto aos benefícios')
+    print()
+
+print('---------------------------------------------')
+
+dano = 15
+escudo = 0
+
+if(dano > 10 and escudo == 0):
+ print('Você está morto')
+ print()
+print('---------------------------------------')
+
+norte = True
+sul = False
+leste = False
+oeste = False
+
+if(norte or sul or leste or oeste):
+   print('Você escapou!')
+
+# Resposta alternativa
+#if(norte == True or sul == True or leste == True or oeste == True):
+ #print('Você escapou!')
