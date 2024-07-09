86651017a5111b974835b18df551b82cd4c88667
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-07-09 16:41:02 -0300

    Create condicionais_compostas.py

---
+++ aula03/condicionais_compostas.py
@@ -0,0 +1,17 @@
+ano = 2024
+
+if(ano % 4 == 0):
+    print('Pode ser um ano bissexto')
+else:
+    print('Definitivamente não é um ano bissexto')
+
+print()
+print('----------------------------------------')
+
+cima = True
+baixo = True
+
+if(cima == True and baixo == True):
+    print('Decida-se')
+else:
+    print('Você escolheu um caminho')
