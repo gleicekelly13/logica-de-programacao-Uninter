7c39091b2c98dba5171777bcda1d5b1fcfad57ce
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-07-10 09:28:34 -0300

    Create prob3.py

---
+++ aula03/prob3.py
@@ -0,0 +1,24 @@
+kwh = int(input('Qual a quantidade de kWh consumida? '))
+
+print('Tipos de instalação:\nR - Residência\nC - Comércio\nI - Indústria')
+
+tipo_instalacao = input('Qual o tipo de insalação utilizado?(R, I ou C) ')
+
+if (tipo_instalacao == 'R' and kwh <= 500):
+ preco = kwh * 0.40
+ print (f'Você gastou {kwh}/kwh. Valor a pagar é: {preco:.2f}')
+elif (tipo_instalacao == 'R' and kwh > 500):
+ preco = kwh * 0.65
+ print (f'Você gastou {kwh}/kwh. Valor a pagar é: {preco:.2f}')
+elif (tipo_instalacao == 'C' and kwh <= 1000):
+ preco = kwh * 0.55
+ print (f'Você gastou {kwh}/kwh. Valor a pagar é: {preco:.2f}')
+elif (tipo_instalacao == 'C' and kwh > 1000):
+ preco = kwh * 0.60
+ print (f'Você gastou {kwh}/kwh. Valor a pagar é: {preco:.2f}')
+elif (tipo_instalacao == 'I'):
+ if (kwh <= 5000):
+  preco = kwh * 0.55
+ else:
+  preco = kwh * 0.60
+ print (f'Você gastou {kwh}/kwh. Valor a pagar é: {preco:.2f}')
