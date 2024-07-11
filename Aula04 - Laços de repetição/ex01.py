04eeab821278a53aa1860ec6e1cb0a54fa9cc6b5
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-07-11 15:34:31 -0300

    Create ex01.py

---
+++ Aula04 - Laços de repetição/ex01.py
@@ -0,0 +1,19 @@
+#Exibir 5 vezes enquanto for menor ou igual a 5
+x = 1
+print(x)
+x = x + 1
+print(x)
+x = x + 1
+print(x)
+x = x + 1
+print(x)
+x = x + 1
+print(x)
+
+print('----------------------------------------------')
+
+x = 1
+while(x <= 5):
+    print(x)
+    x = x + 1
+
