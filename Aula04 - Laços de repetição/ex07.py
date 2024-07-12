c016b0354947a521788b93b5b360dc1b42c4f3cc
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-07-12 10:44:22 -0300

    Create ex07.py

---
+++ Aula04 - Laços de repetição/ex07.py
@@ -0,0 +1,20 @@
+x = 3
+while(x <= 12):
+    print(x)
+    x += 1
+
+print('------------------')
+
+for i in range(3, 13):
+    print(i)
+
+print('----------------------')
+x = 0
+while(x < 9):
+    print(x)
+    x += 2
+
+print('------------------')
+
+for i in range(0, 9, 2):
+    print(i)
