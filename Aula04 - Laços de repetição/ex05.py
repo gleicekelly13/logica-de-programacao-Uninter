ea19a2d4841f3912143343b3e0400c2612fd293c
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-07-12 10:34:56 -0300

    Create ex05.py

---
+++ Aula04 - Laços de repetição/ex05.py
@@ -0,0 +1,5 @@
+# Varredura de string
+
+frase = "Lógica de programação e Algoritmos"
+for i in range(0, len(frase), 1): # Inicia em 0 e vai até o tamanho final da string
+    print(frase[i], end= "") # end = ignora o enter e mostra toda a string na mesma linha
