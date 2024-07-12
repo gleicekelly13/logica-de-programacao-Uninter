b8467a07b05c149f5f0c3878431f86d29478360c
Gleice Kelly <80974593+gleicekelly13@users.noreply.github.com>
2024-07-12 10:33:07 -0300

    Create ex04.py

---
+++ Aula04 - Laços de repetição/ex04.py
@@ -0,0 +1,12 @@
+for i in range(6):  #Por padrão, o for inicia em 0, e vai  até o valor final entre parênteses subratindo 1, e assume que vai fazer isso de 1 em 1.
+ print(i)
+
+print('-------------------')
+
+for i in range(1, 6, 2): # Especificando 1 = valor inicial; 6 = valor final do iterador; 1 = passo do iterador
+  print(i)
+
+print('-------------------------')
+
+for i in range(10, 0, -2):
+ print(i)
